2db141055ce436f3c3d716f2c7efb0e34832a936
aceylan6982 <aceylan441@gmail.com>
2022-08-31 10:48:35 +0300

    getUserDataWithAxios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+
+async function getData(num) {
+
+    const { data: getUser } = await axios("https://jsonplaceholder.typicode.com/users/" + num)
+    const { data: getPost } = await axios("https://jsonplaceholder.typicode.com/posts?id=" + num)
+
+    return {getUser , getPost}
+
 }
 
-export default App;
+export default getData
